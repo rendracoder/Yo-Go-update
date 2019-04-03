f88b38d24121426af2ee344ce936e606e6992d92
Project343 <35072818+rendracoder@users.noreply.github.com>
2019-04-03 22:05:09 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,15 @@
+<?php
+
+/* SETTING CONFIG */
+/* $sleep jangan di ubah ! */
+$sleep=40;
+/* CONFIG DATA */
+
+$sign="xxxxx";
+$data="xxxxx";
+
+/* DATA CONFIG WATCH VIDEO */
+
+$data_body="xxxxxx";
+
+?>
